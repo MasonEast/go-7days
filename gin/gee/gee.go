f5a2721d902dfa0bb0de1e574c4921520a2184f3
mason <2224742726@qq.com>
2022-04-29 11:01:36 +0800

    gin： 封装context和router

--- gin/gee/gee.go
+++ gin/gee/gee.go
@@ -1,26 +1,36 @@
 package gee
 
+/*
+实现思路：
+1. 从http.ListenAndServer的第二参数入手，这个参数从net/http源码可以了解，它是一个实现了ServeHTTP方法的接口，所以这里
+只要我们自己传入一个实现了ServeHTTP的接口实例，就可以拿到请求自行处理；
+2. 创建一个Engine的空结构体，并实现ServeHTTP方法，让它作为http.ListenAndServer的第二参数；
+3. ServeHTTP方法接收两个参数：w http.ResponseWriter, req *http.Request，我们可以抽出一个Context，用来接收这两个参数
+并做一些封装处理；
+4. Context可以根据这两个参数实现，获取请求参数，对外暴露设置接口返回头，状态，返回体的方法（String，JSON， Data， HTML等）
+5. 封装Engine，想让它可以处理各种请求可以抽出Router；
+6. router主要是实现添加各种请求，并维护一个每种请求对应handler的map；
+
+
+*/
 import (
-	"fmt"
-	
 	"net/http"
 )
 
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(*Context)
 
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 func New() *Engine {
 	return &Engine{
-		router: make(map[string]HandlerFunc),
+		router: newRouter(),
 	}
 }
 
 func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	e.router[key] = handler
+	e.router.addRoute(method, pattern, handler)
 }
 
 func (e *Engine) GET(pattern string, handler HandlerFunc) {
@@ -31,15 +41,11 @@ func (e *Engine) POST(pattern string, handler HandlerFunc) {
 	e.addRoute("POST", pattern, handler)
 }
 
-func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
-}
-
 func (e *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, e)
+}
+
+func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	e.router.handle(c)
 }
--- gin/main.go
+++ gin/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"gee"
@@ -9,15 +8,9 @@ import (
 
 func main() {
 	r := gee.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>hello</h1>")
 	})
-
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
-	})
-
+	
 	r.Run(":9999")
 }
