1b9dbb43b197041c9c2d508b8ccb75ab0a7c905e
mason <2224742726@qq.com>
2022-04-30 10:11:53 +0800

    gin: 添加前缀树处理动态路由

--- gin/gee/router.go
+++ gin/gee/router.go
@@ -3,29 +3,92 @@ package gee
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
+// handlers用于存储每种请求的处理方法
+// roots存储每种请求的前缀树的根节点
 type router struct {
 	handlers map[string]HandlerFunc
+	roots map[string]*node
 }
 
 func newRouter() *router {
 	return &router{
 		handlers: make(map[string]HandlerFunc),
+		roots: make(map[string]*node),
 	}
+}
+
+// 解析路径，存储成数组
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			// 通配符情况下，后面的路径就不需要记录了
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
 }
 
 func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	parts := parsePattern(pattern)
+
 	log.Printf("Route %4s - %s", method, pattern)
 	key := method + "-" + pattern
+	_, ok := r.roots[method]
+
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
 	r.handlers[key] = handler
 }
 
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+
+	if !ok {
+		return nil, nil
+	}
+
+	n := root.search(searchParts, 0)
+
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[index]
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+
+	return nil, nil
+}
+
 func (r *router) handle(c *Context) {
-	key := c.Method + "-" + c.Path
-	if handler, ok := r.handlers[key]; ok {
-		handler(c)
+	n, params := r.getRoute(c.Method, c.Path)
+
+	if n != nil {
+		// 将路由参数给到context，方便后续直接从context中取值
+		c.Params = params
+		key := c.Method + "-" + n.pattern
+		r.handlers[key](c)
 	} else {
 		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
 	}
+
 }
---
+++ gin/gee/trie.go
@@ -0,0 +1,72 @@
+package gee
+
+import "strings"
+
+// 前缀树节点
+type node struct {
+	pattern string	// 待匹配路由
+	part string 		// 路由中的一部分
+	children []*node	// 子节点
+	isWild bool			// 是否精确匹配
+}
+
+// 第一个匹配成功的节点，用于插入
+func (n *node) matchChild(part  string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+// 所有匹配成功的节点，用于查找
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
+
+// 通过递归将所有的子节点存进来
+func (n *node) insert(pattern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		child = &node{
+			part: part,
+			isWild: part[0] == ':' || part[0] == '*',
+		}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern, parts, height + 1)
+}
+
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+
+	part := parts[height]
+	children := n.matchChildren(part)
+
+	for _, child := range children {
+		result := child.search(parts, height + 1)
+		if result != nil {
+			return result
+		}
+	}
+
+	return nil
+}
