813d470e3b7c690344503ee3cb0e09248d9156e3
mason <2224742726@qq.com>
2022-05-07 15:03:20 +0800

    orm：支持数据库迁移

--- orm/orm.go
+++ orm/orm.go
@@ -14,9 +14,12 @@ orm思路：
 7. 为复杂sql语句构建生成器clause；
 8. 提供钩子支持在数据库操作时做一些用户自定义化的操作；
 9. 增加sql.Tx支持事务；
+10. 支持数据库迁移；
 */
 import (
 	"database/sql"
+	"fmt"
+	"strings"
 
 	"orm/dialect"
 	"orm/log"
@@ -68,7 +71,7 @@ func (engine *Engine) NewSession() *session.Session {
 
 type TxFunc func(*session.Session) (interface{}, error)
 
-// Transaction executes sql wrapped in a transaction, then automatically commit if no error occurs
+// 事务
 func (engine *Engine) Transaction(f TxFunc) (result interface{}, err error) {
 	s := engine.NewSession()
 	if err := s.Begin(); err != nil {
@@ -86,4 +89,55 @@ func (engine *Engine) Transaction(f TxFunc) (result interface{}, err error) {
 	}()
 
 	return f(s)
+}
+
+func difference(a []string, b []string) (diff []string) {
+	mapB := make(map[string]bool)
+	for _, v := range b {
+		mapB[v] = true
+	}
+	for _, v := range a {
+		if _, ok := mapB[v]; !ok {
+			diff = append(diff, v)
+		}
+	}
+	return
+}
+
+// 迁移
+func (engine *Engine) Migrate(value interface{}) error {
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		if !s.Model(value).HasTable() {
+			log.Infof("table %s doesn't exist", s.RefTable().Name)
+			return nil, s.CreateTable()
+		}
+
+		table := s.RefTable()
+		rows, _ := s.Raw(fmt.Sprintf("SELECT * FROM %s LIMIT 1", table.Name)).QueryRows()
+		columns, _ := rows.Columns()
+		addCols := difference(table.FieldNames, columns)
+		delCols := difference(columns, table.FieldNames)
+
+		log.Infof("added cols %v, deleted cols %v", addCols, delCols)
+
+		for _, col := range addCols {
+			f := table.GetField(col)
+			sqlStr := fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s %s;", table.Name, f.Name, f.Type)
+			if _, err = s.Raw(sqlStr).Exec(); err != nil {
+				return
+			}
+		}
+
+		if len(delCols) == 0 {
+			return
+		}
+		tmp := "tmp_" + table.Name
+		fieldStr := strings.Join(table.FieldNames, ", ")
+		s.Raw(fmt.Sprintf("CREATE TABLE %s AS SELECT %s from %s;", tmp, fieldStr, table.Name))
+		s.Raw(fmt.Sprintf("DROP TABLE %s;", table.Name))
+		s.Raw(fmt.Sprintf("ALTER TABLE %s RENAME TO %s;", tmp, table.Name))
+		_, err = s.Exec()
+		return
+	})
+	return err
 }
